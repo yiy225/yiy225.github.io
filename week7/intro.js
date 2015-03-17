8f88520fb90198faabcbc4cd5f7e47840a6b8862
Your Name <you@example.comyiy225>
2015-03-17 16:49:06 -0400

    week7

---
+++ week7/intro.js
@@ -0,0 +1,8 @@
+var name = prompt("What is your name?");
+document.getElementById("xholder").innerHTML = name;
+
+var color = prompt("What is your favorite color?");
+document.getElementById("yholder").innerHTML = color;
+
+var birthday = prompt("What is your birthday?");
+document.getElementById("zholder").innerHTML = birthday;
